3f9617dd49d2951c32377575be5ed82d0480a107
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-02-12 11:54:04 +0100

    Empty project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Contador: 0</h1>
+      <button>Incrementar</button>
+      <p/>
+      <button>Decrementar</button>
+    </>
+  )
+}
+
+export default App
